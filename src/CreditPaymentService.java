dea96918408580ba1bf2ddb918405cdf0b30698e
nhegm <nhegmus@gmail.com>
2023-04-21 23:00:37 +0800

    parameters renamed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-    public double calculate(double a, double p, int y) {
-        int m = y * 12;
-        double intRate = p / (100 * 12);
-        double result = a * intRate / (1 - Math.pow(1 + intRate, -m));
+    public double calculate(double amount, double percentage, int term) {
+        int months = term * 12;
+        double intRate = percentage / (100 * 12);
+        double result = amount * intRate / (1 - Math.pow(1 + intRate, -months));
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         int term1 = 1;                   // credit term (years)
         int term2 = 2;                   // credit term (years)
         int term3 = 3;                   // credit term (years)
-        double credit1 = service.calculate(amount, percentage, term1);
-        double credit2 = service.calculate(amount, percentage, term2);
-        double credit3 = service.calculate(amount, percentage, term3);
-        int result1 = (int) credit1;
-        int result2 = (int) credit2;
-        int result3 = (int) credit3;
+        double credit1year = service.calculate(amount, percentage, term1);
+        double credit2years = service.calculate(amount, percentage, term2);
+        double credit3years = service.calculate(amount, percentage, term3);
+        int result1 = (int) credit1year;
+        int result2 = (int) credit2years;
+        int result3 = (int) credit3years;
         System.out.println("Ежемесячный размер платежа по кредиту: " + result1);
         System.out.println("Ежемесячный размер платежа по кредиту: " + result2);
         System.out.println("Ежемесячный размер платежа по кредиту: " + result3);
